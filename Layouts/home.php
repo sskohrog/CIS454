70081835d27239744c25069a3f63d361c81067d5
Sophie <sskohrog@syr.edu>
2017-03-07 01:07:28 -0500

    working

--- Layouts/home.php
+++ Layouts/home.php
@@ -1,6 +1,6 @@
 <div>
 	HELLO WORLD!!!
 	</br>
-	<a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a>
+	<a href="logout.php?logout">&nbsp;Sign Out</a>
 </div>
 
