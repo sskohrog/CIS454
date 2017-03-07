ef0a22a2484cfefc27afd61eb7f3f9218987b0b2
Sophie <sskohrog@syr.edu>
2017-03-07 00:37:01 -0500

    working...?

--- Layouts/Login-Signup/Login_Register.php
+++ Layouts/Login-Signup/Login_Register.php
@@ -26,6 +26,18 @@ if( isset($_POST['action']=="signup") {
   $pass = strip_tags($pass);
   $pass = htmlspecialchars($pass);
 
+// basic name validation
+  if (empty($name)) {
+    $error = true;
+    $nameError = "Please enter your full name.";
+  } else if (strlen($name) < 3) {
+    $error = true;
+    $nameError = "Name must have atleat 3 characters.";
+  } else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+    $error = true;
+    $nameError = "Name must contain alphabets and space.";
+  }
+
 //basic email validation
   if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
     $error = true;
@@ -39,6 +51,14 @@ if( isset($_POST['action']=="signup") {
       $error = true;
       $emailError = "Provided Email is already in use.";
     }
+  } 
+// password validation
+  if (empty($pass)){
+    $error = true;
+    $passError = "Please enter password.";
+  } else if(strlen($pass) < 6) {
+    $error = true;
+    $passError = "Password must have atleast 6 characters.";
   }
 
   $password = hash('sha256', $pass);
