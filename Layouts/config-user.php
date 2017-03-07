98d9930810ab257bc62f908f4c164653ccabed3f
Sophie <sskohrog@syr.edu>
2017-03-06 23:54:27 -0500

    testing database3

--- Layouts/config-user.php
+++ Layouts/config-user.php
@@ -5,7 +5,7 @@ Configures the database info
 <?php
 	$DB_SERVER = 'localhost';
 	$DB_USERNAME = 'ec2-user';
-	$DB_PASSWORD = 'password';
+	$DB_PASSWORD = 'scheduler';
 	$DB_DATABASE = 'secure_login';
 	 
 	$conn = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
