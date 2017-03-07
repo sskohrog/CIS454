66ab99cb1578405a119dbdaacb8ff668a3c98233
Sophie <sskohrog@syr.edu>
2017-03-07 01:28:31 -0500

    working

--- Layouts/home.php
+++ Layouts/home.php
@@ -1,6 +1,19 @@
+<?php
+ ob_start();
+ session_start();
+ require_once 'dbconnect.php';
+ 
+ if( !isset($_SESSION['user']) ) {
+  header("Location: index.php");
+  exit;
+ }
+
+ $res=mysql_query("SELECT * FROM members WHERE id=".$_SESSION['user']);
+ $userRow=mysqli_fetch_array($res);
+?>
+
 <div>
-	HELLO WORLD!!!
+	HELLO <?php echo $userRow['username']; ?> !!!
 	</br>
 	<a href="logout.php?logout">&nbsp;Sign Out</a>
 </div>
-
