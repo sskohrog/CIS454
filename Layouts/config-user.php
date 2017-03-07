1f299898e78c93a33e879ae5090f7963514b0022
Sophie <sskohrog@syr.edu>
2017-03-06 23:53:17 -0500

    testing database3

--- Layouts/config-user.php
+++ Layouts/config-user.php
@@ -5,18 +5,18 @@ Configures the database info
 <?php
 	$DB_SERVER = 'localhost';
 	$DB_USERNAME = 'ec2-user';
-	$DB_PASSWORD = 'password';
+	$DB_PASSWORD = 'scheduler';
 	$DB_DATABASE = 'secure_login';
 	 
 	$conn = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 	$dbcon = mysqli_select_db(DB_DATABASE);
 	 
 	if ( !$conn ) {
-		die("Connection failed : " . mysql_error());
+		die("Connection failed : " . mysqli_error());
 	}
 	 
 	if ( !$dbcon ) {
-	 	die("Database Connection failed : " . mysql_error());
+	 	die("Database Connection failed : " . mysqli_error());
 	}
 
 ?>
