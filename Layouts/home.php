29fca46c848316313acd8e319d431c4d68fa1f57
Sophie <sskohrog@syr.edu>
2017-03-07 01:29:57 -0500

    workingsudo git add --allsudo git add --all

--- Layouts/home.php
+++ Layouts/home.php
@@ -1,10 +1,10 @@
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
+include_once "config-user.php";
+ob_start();
+session_start();
  
  if( !isset($_SESSION['user']) ) {
-  header("Location: index.php");
+  header("Location: Login-Signup/Login_Register.php");
   exit;
  }
 
